a9783f747542a24dc43cedf4ff4d68c243c98741
matthewWojcik187 <73750004+matthewWojcik187@users.noreply.github.com>
2023-03-13 14:31:32 -0400

    Update archivepost-steps.py

--- tests/features/steps/archivepost-steps.py
+++ tests/features/steps/archivepost-steps.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 def open_browser(context):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
     context.driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     context.driver.implicitly_wait(5)
     

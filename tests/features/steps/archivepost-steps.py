2e51f57a8c204dae20cae985a1c772aaa19d3c11
matthewWojcik187 <73750004+matthewWojcik187@users.noreply.github.com>
2023-03-13 14:40:07 -0400

    Update archivepost-steps.py

--- tests/features/steps/archivepost-steps.py
+++ tests/features/steps/archivepost-steps.py
@@ -10,6 +10,7 @@ def open_browser(context):
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     context.driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     context.driver.implicitly_wait(5)
     
